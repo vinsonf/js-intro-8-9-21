91fb63d03826e31f249f5d2a05eb9990e0ceacd3
vinsonf <vfernandez@bitwiseindustries.com>
2021-08-16 19:15:05 -0700

    test

---
+++ day03/js/data-types.js
@@ -0,0 +1,41 @@
+// Create, Read, Update, Delete
+
+/* 
+    Data types
+    (information)
+
+*/
+
+// 3 types of variables
+// simple or primitive
+let score = 0; // number
+let name = 'Vinson'; // string
+let isPresent = true; // boolean
+
+
+// complex/compound 
+const array = [score, name, isPresent];
+const object = {
+    name: 'Vinson',
+    isPresent: true,
+    innerText: 'Click'
+};
+
+
+
+
+
+
+
+
+/*
+
+1. declare ( makes a place in memory to hold variable)/ all
+2. assign data to the variable
+2. re-assign = var, let.
+
+
+*/
+
+
+
