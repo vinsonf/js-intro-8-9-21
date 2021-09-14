f7a368455c167e287423fb45c6a22ce634ba0dba
vinsonf <vfernandez@shift3tech.com>
2021-09-13 20:14:12 -0700

    math

--- day11/js/main.js
+++ day11/js/main.js
@@ -8,3 +8,11 @@ style(h1, 'background-color', 'black');
 listen(h1, 'click', () => { alert('click')});
 toggle(h1, 'text-center');
 append(query('p'), h1)
+
+const randomNumber = Math.random();
+//                 0  ,  1  ,   2    ,       3,             4
+const answers = ['yes', 'no', 'maybe', 'ask again later', 'idk'];
+const numberMultiplied = randomNumber * answers.length;
+const floored = Math.floor(numberMultiplied);
+const fixed = numberMultiplied.toFixed(0);
+innerHTML(h1, answers[floored]);
